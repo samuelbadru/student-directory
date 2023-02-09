0dbd52121afb113288a7c3df2e43975346abbe6b
samuelbadru <samuelbadru@outlook.com>
2023-02-09 21:42:37 +0000

    Refactoring - created the instance variables @students to store array and the print_menu, process and show_students methods to simplify interactive menu method

--- directory.rb
+++ directory.rb
@@ -1,43 +1,52 @@
-# Method to ask and execute what the user wants to do
-def interactive_menu
-    students = []
-    loop do
+@students = []
+
+def print_menu
     puts "1. Input student information"
     puts "2. Display all students' information"
     puts "3. Search for a student's information using their initals"
     puts "4. Search for student information based on name length"
     puts "5. Fix a typo"
     puts "9. Exit"
+end
+
+
+def interactive_menu
+    loop do
     
     selection = gets.chomp
+    process(gets.chomp)
     
-    case selection
-        when "1" then students = input_students
-        when "2" then print_header ; print(students) ; print_footer(students)
-        when "3" then search_initials(students)
-        when "4" then search_length(students)
-        when "5" then fix_typo(students)
-        when "9" then puts "Goodbye" ; return
-        else puts "I don't know what you meant, try again" 
-    end
     end
 end
 
+def process(selection)
+    case selection
+        when "1" then input_students
+        when "2" then show_students
+        when "3" then search_initials
+        when "4" then search_length
+        when "5" then fix_typo
+        when "9" then puts "Goodbye" ; return
+        else puts "I don't know what you meant, try again" 
+    end
+end
+
+
+def show_students
+    print_header ; print_students ; print_footer
+end
 
 # Method to create the students array
 def input_students
     
     puts "Please enter the students' information"
-    
-    # Empty array
-    students = []
     i = 1
     
     while true
         puts "Enter the first and last names of student #{i} or hit return to exit"
         name = gets.chomp
         if name.empty?
-            return students
+            return
         end
         
         puts "Enter the age of #{name}"
@@ -71,14 +80,12 @@ def input_students
         end
         
         # add student hash to the students array
-        students << {name: name, age: age, cohort: cohort, birth: birth, 
+        @students << {name: name, age: age, cohort: cohort, birth: birth, 
         height: height, hobbies: hobbies}
-        puts "Now we have #{students.count} students"
+        puts "Now we have #{@students.count} students"
         
         i += 1
     end
-    # implicitly returns the students array
-    
 end
 
 # Method to print header
@@ -88,28 +95,28 @@ def print_header
 end
 
 # Method to print names and cohort month in a given nested array
-def print(students)
+def print_students
     i = 0
-    while i < students.length
-        puts "#{i + 1}. #{students[i][:name]} (Age: #{students[i][:age]}, Cohort: #{students[i][:cohort]}, Birth Country: #{students[i][:birth]}, Height: #{students[i][:height]}, Hobbies: #{students[i][:hobbies]})"
+    while i < @students.length
+        puts "#{i + 1}. #{@students[i][:name]} (Age: #{@students[i][:age]}, Cohort: #{@students[i][:cohort]}, Birth Country: #{@students[i][:birth]}, Height: #{@students[i][:height]}, Hobbies: #{@students[i][:hobbies]})"
         i += 1
     end
 end
 
 # Method to print the footer, which contains the count of the given array
-def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+def print_footer
+    puts "Overall, we have #{@students.count} great students"
 end
 
 # Method to search for a user given their initials
-def search_initials(students)
+def search_initials
     
     while true
         puts "Enter the initials for the student you are looking for or hit return to exit"
         results = []
         initials = gets.chomp.upcase
         
-        students.each_with_index do |student, i|
+        @students.each_with_index do |student, i|
             names = student[:name].upcase.split()
             if initials == "#{names[0][0]}#{names[1][0]}"
                 results << "#{i + 1}. #{student[:name]} (Age: #{student[:age]}, Cohort: #{student[:cohort]}, Birth Country: #{student[:birth]}, Height: #{student[:height]}, Hobbies: #{student[:hobbies]})"
@@ -130,13 +137,13 @@ def search_initials(students)
     end
 end
 
-def search_length(students)
+def search_length
     while true
         puts "Enter the max character length of the name or hit return to exit"
         results = []
         length = gets.chomp
         
-        students.each_with_index do |student, i|
+        @students.each_with_index do |student, i|
             if student[:name].length <= length.to_i
                 results << "#{i + 1}. #{student[:name]} (Age: #{student[:age]}, Cohort: #{student[:cohort]}, Birth Country: #{student[:birth]}, Height: #{student[:height]}, Hobbies: #{student[:hobbies]})"
             end
@@ -154,7 +161,7 @@ def search_length(students)
     end
 end
 
-def fix_typo(students)
+def fix_typo
     while true
         puts "Enter the name of the student you wish to edit or hit return to exit"
         name = gets.chomp
@@ -164,7 +171,7 @@ def fix_typo(students)
             return
         end
         
-        students.each do |student|
+        @students.each do |student|
             if student[:name].downcase == name.downcase
                 x += 1
             end
@@ -179,7 +186,7 @@ def fix_typo(students)
         puts "Enter the category you wish to edit"
         cat = gets.chomp.downcase.to_sym
         
-        if !students[0].key?(cat)
+        if !@students[0].key?(cat)
             puts "Incorrect category"
             next
         end
@@ -188,7 +195,7 @@ def fix_typo(students)
         fix = gets.chomp
         
         # Not recognising category, even when correct
-        students.each_with_index do |student, i|
+        @students.each_with_index do |student, i|
             if student[:name] == name
                 student[cat] = fix
                 puts "#{i + 1}. #{student[:name]} (Age: #{student[:age]}, Cohort: #{student[:cohort]}, Birth Country: #{student[:birth]}, Height: #{student[:height]}, Hobbies: #{student[:hobbies]})"
