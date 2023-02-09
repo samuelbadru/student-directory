6cd2b63e9509636cff6096206567485c712b37bb
samuelbadru <samuelbadru@outlook.com>
2023-02-09 19:58:39 +0000

    Created fix_typo method as well as implemented a goodbye message when exiting the program

--- directory.rb
+++ directory.rb
@@ -11,21 +11,42 @@ def input_students
         puts "Enter the first and last names of student #{i} or hit return to exit"
         name = gets.chomp
         if name.empty?
+            puts "Goodbye!"
             break
         end
+        
         puts "Enter the age of #{name}"
         age = gets.chomp
+        if age.empty?
+            age = "N/A"
+        end
+        
         puts "Enter the cohort of #{name}"
         cohort = gets.chomp
+        if cohort.empty?
+            cohort = "N/A"
+        end
+        
         puts "Enter the country of birth of #{name}"
         birth = gets.chomp
+        if birth.empty?
+            birth = "N/A"
+        end
+        
         puts "Enter the height of #{name} in cm"
         height = gets.chomp
+        if height.empty?
+            height = "N/A"
+        end
+        
         puts "Enter the hobbies of #{name}"
         hobbies = gets.chomp
+        if hobbies.empty?
+            hobbies = "N/A"
+        end
         
         # add student hash to the students array
-        students << {name: name, age: age, cohort: cohort, birth: birth, 
+        students << {name: name, age: age, cohort: cohort.to_sym, birth: birth, 
         height: height, hobbies: hobbies}
         puts "Now we have #{students.count} students"
         
@@ -59,20 +80,20 @@ end
 def search_initials(students)
     
     while true
-        puts "Enter the initials for the student you are looking for"
-        puts "To finish, hit return twice"
+        puts "Enter the initials for the student you are looking for or hit return to exit"
         results = []
         initials = gets.chomp.upcase
         
         students.each_with_index do |student, i|
             names = student[:name].upcase.split()
             if initials == "#{names[0][0]}#{names[1][0]}"
-                results << "#{i + 1}. #{students[i][:name]} (Age: #{students[i][:age]}, Cohort: #{students[i][:cohort]}, Birth Country: #{students[i][:birth]}, Height: #{students[i][:height]}, Hobbies: #{students[i][:hobbies]})"
+                results << "#{i + 1}. #{student[:name]} (Age: #{student[:age]}, Cohort: #{student[:cohort]}, Birth Country: #{student[:birth]}, Height: #{student[:height]}, Hobbies: #{student[:hobbies]})"
             end
         end
         
         # If user hits return when prompted, then method concludes
         if initials.empty?
+            puts "Goodbye!"
             break
         elsif results.empty?
             puts "No match found!"
@@ -87,18 +108,18 @@ end
 
 def search_length(students)
     while true
-        puts "Enter the max character length of the name"
-        puts "To finish, hit return twice"
+        puts "Enter the max character length of the name or hit return to exit"
         results = []
         length = gets.chomp
         
         students.each_with_index do |student, i|
             if student[:name].length <= length.to_i
-                results << "#{i + 1}. #{students[i][:name]} (Age: #{students[i][:age]}, Cohort: #{students[i][:cohort]}, Birth Country: #{students[i][:birth]}, Height: #{students[i][:height]}, Hobbies: #{students[i][:hobbies]})"
+                results << "#{i + 1}. #{student[:name]} (Age: #{student[:age]}, Cohort: #{student[:cohort]}, Birth Country: #{student[:birth]}, Height: #{student[:height]}, Hobbies: #{student[:hobbies]})"
             end
         end
         
         if length.empty?
+            puts "Goodbye!"
             break
         elsif results.empty?
             puts "No match found!"
@@ -110,11 +131,57 @@ def search_length(students)
     end
 end
 
+def fix_typo(students)
+    while true
+        puts "Enter the name of the student you wish to edit or hit return to exit"
+        name = gets.chomp
+        x = 0
+        
+        if name.empty?
+            puts "Goodbye!"
+            break
+        end
+        
+        students.each do |student|
+            if student[:name].downcase == name.downcase
+                x += 1
+            end
+        end
+        
+        if x != 1
+            puts "Student does not exist in directory"
+            next
+        end
+            
+        
+        puts "Enter the category you wish to edit"
+        cat = gets.chomp.downcase.to_sym
+        
+        if !students[0].key?(cat)
+            puts "Incorrect category"
+            next
+        end
+        
+        puts "Enter your correction"
+        fix = gets.chomp
+        
+        # Not recognising category, even when correct
+        students.each_with_index do |student, i|
+            if student[:name] == name
+                student[cat] = fix
+                puts "#{i + 1}. #{student[:name]} (Age: #{student[:age]}, Cohort: #{student[:cohort]}, Birth Country: #{student[:birth]}, Height: #{student[:height]}, Hobbies: #{student[:hobbies]})"
+            end
+        end
+        
+    end
+end
 # Calling methods
 
 students = input_students
+
 print_header
 print(students)
 print_footer(students)
 #search_initials(students)
 #search_length(students)
+fix_typo(students)
